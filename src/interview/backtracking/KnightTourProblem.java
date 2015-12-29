5128d41d32bc62532868a6c8f0dfa5de8e67857c
prit91 <pncooldude91@gmail.com>
2015-12-30 02:19:40 +0530

    Added problem description in knights problem

--- src/interview/backtracking/KnightTourProblem.java
+++ src/interview/backtracking/KnightTourProblem.java
@@ -1,5 +1,11 @@
 package interview.backtracking;
 
+/*
+ * A knight's tour is a sequence of moves of a knight on a chessboard such that the 
+ * knight visits every square only once. If the knight ends on a square that is one knight's
+ *  move from the beginning square (so that it could tour the board 
+ * again immediately, following the same path), the tour is closed, otherwise it is open.
+ */
 public class KnightTourProblem {
 private static final int numSides = 8;
 
